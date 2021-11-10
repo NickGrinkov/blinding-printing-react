092199e4633baa59addd03f1ed5c82c9f20e18e8
NickGrinkov <nikitagr1801@gmail.com>
2021-11-10 11:42:42 +0300

    created Timer

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
-import { Navigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import './Main.scss';
 import logo from '../../assets/blind-printing.jpg';
 
 function Main() {
+    const navigate = useNavigate()
+
+    const getStart = () => {
+        navigate('/test')
+    }
     return (
         <div className="main">
             <img src={logo} alt="logo"/>
             <h1>Приготовьтесь печатать</h1>
-            <button className="main__button">Начать печатать</button>
+            <button onClick={() => getStart()} className="main__button">Начать печатать</button>
         </div>
     )
 }
--- src/components/Testing/Test.jsx
+++ src/components/Testing/Test.jsx
@@ -1,14 +1,47 @@
-import React from 'react'
+import React from 'react';
+import {useState, useEffect} from 'react';
+import {Link} from 'react-router-dom';
 import './Test.scss';
 
 function Test() {
+
+    const [seconds, setSeconds] = useState(0)
+    const [isActive, setIsActive] = useState(false)
+
+    const startTimer = () => {
+        setIsActive(true)
+    }
+
+    const clearTimer = () => {
+        setIsActive(false)
+        setSeconds(0)
+    }
+    
+    useEffect(() => {
+        let interval;
+        if(isActive) {
+            interval = setInterval(() => setSeconds(seconds => seconds + 1), 1000)
+        }
+        return () => clearInterval(interval)
+    }, [isActive, seconds])
+
     return (
         <div className="test">
             <div>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Illo, repellat quis! Optio vitae nihil eveniet expedita rem, saepe voluptatibus eos.
+            <h2>Тест на скорость печати</h2>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Illo, repellat quis! Optio vitae nihil eveniet expedita rem, saepe voluptatibus eos.  
+                </p>
+                <textarea onBlur={() => clearTimer()} onFocus={() => startTimer()}></textarea>
             </div>
             <div className="test__wrapper">
+                <div className="timer">
+                    <div className="speed__wrapper">
+                        <h3>Таймер</h3>
+                        <span>{seconds}</span><span>сек.</span>
+                    </div>
+                </div>
                 <div className="speed">
                     <div className="speed__wrapper">
                         <img src="http://cdn.onlinewebfonts.com/svg/download_536753.png" alt="" />
@@ -25,6 +58,12 @@ function Test() {
                     </div>
                     <p>100%</p>
                 </div>
+                <Link to="/">
+                    <div className="back">
+                        <img src="https://www.clipartmax.com/png/middle/423-4237996_return-svg-png-icon-free-download-onlinewebfonts-return-back-icon.png" alt="" />
+                        <p>Вернуться назад</p>
+                    </div>
+                </Link>
                 <div className="start-again">
                     <img src="https://www.clipartmax.com/png/middle/423-4237996_return-svg-png-icon-free-download-onlinewebfonts-return-back-icon.png" alt="" />
                     <a href="#">Заново</a>
